bb8e5c6d5a3bab787add82224750c3daab27c09b
HMPIWD <129892725+HMPIWD@users.noreply.github.com>
2023-07-23 18:26:53 +0300

    Create HoldPointL.lua
    That thing will hold left point on a blocker map Copy Love Box

---
+++ HoldPointL.lua
@@ -0,0 +1,36 @@
+include(".mlib/main")
+include(".mlib/controls")
+include(".mlib/other")
+
+Game.Console:ExecuteLine("kill", -1, true
+
+local Point = vec2(89.50 * 32, 79.53 * 32)
+
+function OnSnapInput()
+    ResetInput()
+
+    SetWeapon(1)
+
+    local ID = GetClosestPlayer(380, false, false, false, false)
+
+    local TargetPos = Game.Players(ID).Tee.Pos
+    local TargetVel = Game.Players(ID).Tee.Vel
+    local TargetPosPred = TargetPos + TargetVel * vec2(3.6, 3.6)
+   
+    if LocalPosPred.x < Point.x then Right() end
+    if LocalPosPred.x > Point.x then Left() end
+    if LocalPosPred.y > Point.y + 1 then Jump() end
+    if LocalPosPred.y < Point.y + 1 then NoJump() end
+
+    if Game.Players(ID).Tee.Pos.y < Game.LocalTee.Pos.y then Fire() end
+    if Game.Players(ID).Tee.Pos.y > Game.LocalTee.Pos.y then NoFire() end
+    
+    if IsFreeze(Game.LocalTee.Pos.x, Game.LocalTee.Pos.y + 32) then Jump() end
+
+    if ID == -1 then return end
+    if ID == Teams[0] then return end
+
+    SetAimVec(TargetPosPred - Game.LocalTee.Pos)
+    Hook()
+    if Game.LocalTee.HookState == -1 then NoHook() end
+end
